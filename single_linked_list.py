f6ae2bcff620c8730c3863af7321fc092667e580
Prasanth Vinnakota <crazyprasanth16@gmail.com>
2019-05-26 07:30:52 +0530

    complete single linked list code

--- single_linked_list.py
+++ single_linked_list.py
@@ -34,6 +34,11 @@ class SingleLinkedList:
     def insert_at_end(self, data):
         new_node = Node()
         new_node.set_data(data)
+        if self.length == 0:
+            self.head = new_node
+            self.length += 1
+            return
+
         current_node = self.head
         while current_node.get_next() is not None:
             current_node = current_node.get_next()
@@ -64,15 +69,16 @@ class SingleLinkedList:
         current_node.set_next(new_node)
         self.length += 1
 
-    def display_list(self):
+    def get_list(self):
         if self.length == 0:
-            print("\nEmpty list\n")
-            return
+            return "Empty list"
+
+        elements = []
         current_node = self.head
         while current_node is not None:
-            print(current_node.get_data(), end=" ")
+            elements.append(current_node.get_data())
             current_node = current_node.get_next()
-        print()
+        return elements
 
     def search(self, data):
         current_node = self.head
@@ -99,17 +105,136 @@ class SingleLinkedList:
 
         return current_node.get_data()
 
+    def delete_at_beginning(self):
+        if self.get_length() == 0:
+            print("\nNo elements in list")
+            return
+        self.head = self.head.get_next()
+        self.length -= 1
 
-sll = SingleLinkedList()
-sll.insert_at_beginning("nani")
-sll.insert_at_end(11)
-sll.insert_at_beginning(20)
-sll.insert_at_end(10)
-sll.insert_at(15, 2)
-sll.insert_at(16, 4)
-sll.display_list()
-sll.search(10)
-sll.search(16)
-sll.search("nani")
-print(sll.get_data_at(1))
+    def delete_at_end(self):
+        if self.length == 0:
+            print("\nNo elements in list")
+            return
 
+        if self.length == 1:
+            self.head = None
+            self.length -= 1
+            return
+
+        current_node = self.head
+        previous_node = current_node
+        current_node = current_node.get_next()
+
+        while current_node.get_next() is not None:
+            previous_node = previous_node.get_next()
+            current_node = current_node.get_next()
+
+        previous_node.set_next(None)
+        self.length -= 1
+
+    def delete_at(self, position):
+        if position == 0:
+            self.delete_at_beginning()
+            return
+
+        if position >= self.length:
+            self.delete_at_end()
+            return
+
+        current_position = 1
+        current_node = self.head
+        previous_node = current_node
+        current_node = current_node.get_next()
+
+        while current_position != position:
+            previous_node = previous_node.get_next()
+            current_node = current_node.get_next()
+            current_position += 1
+
+        previous_node.set_next(current_node.get_next())
+        self.length -= 1
+
+    def get_reversed_list(self):
+        if self.length == 0:
+            return "Empty list"
+
+        rev = self.get_list()
+        list.reverse(rev)
+        return rev
+
+    def get_sorted_list(self):
+        if self.length == 0:
+            return "Empty list"
+
+        sort = self.get_list()
+        list.sort(sort)
+        return sort
+
+
+def main():
+    sll = SingleLinkedList()
+    while True:
+        try:
+            ch = int(input("\n1.Insert at beginning"
+                           "\n2.Insert at end"
+                           "\n3.Insert at given position"
+                           "\n4.Delete at beginning"
+                           "\n5.Delete at end"
+                           "\n6.Delete at given position"
+                           "\n7.Display list"
+                           "\n8.Search list"
+                           "\n9.Length of list"
+                           "\n10.Display sorted list"
+                           "\n11.display reversed list"
+                           "\n12.Exit"
+                           "\n\nChoose a option: "))
+        except ValueError:
+            print("Invalid option selected")
+            continue
+
+        if ch == 1:
+            data = input("Enter data to insert at beginning of list: ")
+            sll.insert_at_beginning(data)
+        elif ch == 2:
+            data = input("Enter data to insert at ending of list: ")
+            sll.insert_at_end(data)
+        elif ch == 3:
+            try:
+                position = int(input("Enter position to insert data: "))
+            except ValueError:
+                print("Position must be a integer")
+                continue
+
+            data = input("Enter data to insert at position {}: ".format(position))
+            sll.insert_at(data, position)
+        elif ch == 4:
+            sll.delete_at_beginning()
+        elif ch == 5:
+            sll.delete_at_end()
+        elif ch == 6:
+            try:
+                position = int(input("Enter position to insert data: "))
+            except ValueError:
+                print("Position must be a integer")
+                continue
+            sll.delete_at(position)
+        elif ch == 7:
+            print(sll.get_list())
+        elif ch == 8:
+            data = input("Enter data to search: ")
+            sll.search(data)
+        elif ch == 9:
+            print(sll.get_length())
+        elif ch == 10:
+            print(sll.get_sorted_list())
+        elif ch == 11:
+            print(sll.get_reversed_list())
+        elif ch == 12:
+            break
+        else:
+            print("Invalid option selected")
+
+
+if __name__ == '__main__':
+    main()
